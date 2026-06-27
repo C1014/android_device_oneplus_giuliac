7e46b3bae3b1c3480127b3be303f0b37f03b6dcc
ZeroSquared <raphaelquimno@gmail.com>
2026-06-27 20:56:23 +0800

    giuliac: Update proprietary lists and blobs from lunaris

---
+++ lineage_giuliac.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from giuliac device
+$(call inherit-product, device/oneplus/giuliac/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Lunaris Flags
+LUNARIS_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_CUSTOM_UDFPS := true
+TARGET_OPTIMIZED_DEXOPT := true
+TARGET_SUPPORTED_REFRESH_RATES := 60,90,120
+WITH_GMS := true
+WITH_BCR := true
+
+PRODUCT_NAME := lineage_giuliac
+PRODUCT_DEVICE := giuliac
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := PKG110
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="qssi-user 16 BP2A.250605.015 1780929007121 release-keys" \
+    BuildFingerprint=OnePlus/PKG110/OP5D2BL1:16/UKQ1.231108.001/V.2d1b9a2-8e0775-93314e:user/release-keys \
+    DeviceName=OP5D2BL1 \
+    DeviceProduct=PKG110 \
+    SystemDevice=OP5D2BL1 \
+    SystemName=PKG110
